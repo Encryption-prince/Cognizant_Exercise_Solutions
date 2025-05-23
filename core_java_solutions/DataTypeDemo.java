dc9abc487be42ec3eab9239a9d88351080947305
Encryption-prince <subhammaity6827@gmail.com>
2025-05-24 00:44:36 +0530

    DataType Demostration program done

---
+++ core_java_solutions/DataTypeDemo.java
@@ -0,0 +1,22 @@
+package Cognizant_Exercise_Solutions.core_java_solutions;
+
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int a;
+        float b;
+        double c;
+        char d;
+        boolean e;
+        a = 10;
+        b = 14.23f;
+        c = 25.745689d;
+        d = 'd';
+        e = true;
+        System.out.println("Values of the data types are :");
+        System.out.println("int = " + a);
+        System.out.println("float = " + b);
+        System.out.println("double = " + c);
+        System.out.println("char = " + d);
+        System.out.println("boolean = " + e);
+    }
+}
