84bac424fa96533e947965f444383257896d8e34
Encryption-prince <subhammaity6827@gmail.com>
2025-05-24 16:57:00 +0530

    Operator Precidence program done

---
+++ core_java_solutions/OperatorPrec.java
@@ -0,0 +1,8 @@
+package Cognizant_Exercise_Solutions.core_java_solutions;
+
+public class OperatorPrec {
+    public static void main(String[] args) {
+        double res = 4 + 10 / 2 * 3 - (8 / 4);
+        System.out.println("Result of 4+60/2*3-(8/4) expression : " + res);
+    }
+}
