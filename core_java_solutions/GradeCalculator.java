abac44837512fcf06e149fd19894ea261b71ccd5
Encryption-prince <subhammaity6827@gmail.com>
2025-05-25 01:30:33 +0530

    Grade program done

---
+++ core_java_solutions/GradeCalculator.java
@@ -0,0 +1,24 @@
+package Cognizant_Exercise_Solutions.core_java_solutions;
+
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your marks out of 100 : ");
+        int grade = sc.nextInt();
+        if (grade >= 90 && grade <= 100)
+            System.out.println("Grade : A");
+        else if (grade >= 80 && grade <= 89)
+            System.out.println("Grade : B");
+        else if (grade >= 70 && grade <= 79)
+            System.out.println("Grade : C");
+        else if (grade >= 60 && grade <= 69)
+            System.out.println("Grade : D");
+        else if (grade < 60)
+            System.out.println("Grade : F");
+        else
+            System.out.println("Invalid Marks Entered! Please Try Again");
+        sc.close();
+    }
+}
--- core_java_solutions/OperatorPrec.java
+++ core_java_solutions/OperatorPrec.java
@@ -4,5 +4,21 @@ public class OperatorPrec {
     public static void main(String[] args) {
         double res = 4 + 10 / 2 * 3 - (8 / 4);
         System.out.println("Result of 4+60/2*3-(8/4) expression : " + res);
+        /*
+         * Explaination of the Result
+         * --------------------------
+         * BODMAS RULE
+         * 1st op : (8/4) = 2 => Bracket
+         * 2nd op : 10/2 = 5 => Division
+         * 3rd op : 5*3 = 15 => Multiplication
+         * 4th op : 4+15 = 19 => Addition
+         * 5th op : 19-2 = 17 => Substraction
+         * Result : 17
+         * -----------------------------------------------------------------------------
+         * ----
+         * Diviion & Multiplication have same priority (decided by associativity
+         * left->right)
+         * same with Addition & Substraction
+         */
     }
 }
